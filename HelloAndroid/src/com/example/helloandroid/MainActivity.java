fdf511cd671156e3bb81771c6cc7370c11844839
macbookpro <macbookpro@192.168.0.2>
2012-11-04 16:51:38 +0900

    ImageView Sample

--- HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -6,14 +6,57 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import java.io.*;
+import java.net.*;
 
 
 public class MainActivity extends Activity {
 
-    @Override
+    public ImageView mImageView;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btnTest = (Button)findViewById(R.id.button3);
+        btnTest.setOnClickListener(new Button.OnClickListener() {
+        	public void onClick(View v) {
+        		new DownloadImageTask().execute("http://s.nx.com/S2/game/bf/bf_obt/Main_new/bg_cs.gif");
+        	}
+        	
+        });
+        
+        mImageView = (ImageView)findViewById(R.id.imageView1);
+    }
+    
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+	    /** The system calls this to perform work in a worker thread and
+	      * delivers it the parameters given to AsyncTask.execute() */
+	    protected Bitmap doInBackground(String... urls) {
+	        return loadImageFromNetwork(urls[0]);
+	    }	
+	    
+	    /** The system calls this to perform work in the UI thread and delivers
+	      * the result from doInBackground() */
+	    protected void onPostExecute(Bitmap result) {
+	        mImageView.setImageBitmap(result);
+	    }
+	}
+    
+    private Bitmap loadImageFromNetwork(String url) {
+
+    	Bitmap bitmap = null;
+    	
+        try {
+            bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 
     @Override
@@ -34,3 +77,4 @@ public class MainActivity extends Activity {
     	startActivity(intentSub);
     }
 }
+
